93283cb791b6a38bff4b4662561bde5e0531a487
Sarouman54 <sarahbarrabe@gmail.com>
2025-03-24 22:39:10 +0100

    add: init db connection

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const http = require('http');
 const app = require('./app');
+const mongoose = require ('mongoose');
+
+mongoose.connect(process.env.ATLAS_URI)
+    .then(() => console.log('🟢 Connexion à MongoDB réussie !'))
+    .catch((error) => console.log('🔴 Connexion à MongoDB échouée !', process.env.ATLAS_URI));
 
 app.set('port', process.env.PORT || 3000);
 const server = http.createServer(app);
